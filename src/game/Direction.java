9232488318816b0e24c1115d77f9720bfff5b5ee
MithunChandraSarkar <114291327+MithunChandraSarkar@users.noreply.github.com>
2022-09-25 02:14:55 +0600

    Add files via upload

---
+++ src/game/Direction.java
@@ -0,0 +1,11 @@
+package game;
+
+public enum Direction {
+	Up,
+	
+	Right,
+	
+	Down,
+	
+	Left
+}
---
+++ src/game/TileType.java
@@ -0,0 +1,11 @@
+package game;
+
+public enum TileType {
+
+	Fruit,
+	
+	SnakeHead,
+	
+	SnakeBody
+	
+}
